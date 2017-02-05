744a672a4be979c664d4ff334cb35957e0e81aae
yfsoftcom <yfsoftcom@126.com>
2017-02-05 11:14:35 +0800

    run

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var request = require('request');
 var schedule = require('node-schedule');
 var baidu = require('./baidu.js');
 var config = require('./key.js');
+
 
 //*
 var j = schedule.scheduleJob(config.schedule, function(){
@@ -9,3 +9,5 @@ var j = schedule.scheduleJob(config.schedule, function(){
   baidu.checkSite();
 });
 //*/
+
+console.log('baidu pusher running...');
