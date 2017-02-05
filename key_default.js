d48ef90af47d395c3f2ab4a1e5331fe967ae7539
yfsoftcom <yfsoftcom@126.com>
2017-02-05 09:57:56 +0800

    modify urls

--- key_default.js
+++ key_default.js
@@ -16,17 +16,15 @@ module.exports = {
           'http://www.xinyangjlm.com/index.html\n' +
           'http://www.xinyangjlm.com/aboutus/index.html\n' +
           'http://www.xinyangjlm.com/categories/index.html\n' +
-          'http://www.xinyangjlm.com/%E9%98%B2%E7%81%AB%E5%8D%B7%E5%B8%98%E9%97%A8/index.html\n' +
-          'http://www.xinyangjlm.com/%E7%94%B5%E5%8A%A8%E4%BC%B8%E7%BC%A9%E9%97%A8/index.html\n' +
-          'http://www.xinyangjlm.com/%E9%9A%94%E7%A6%BB%E6%A0%8F/index.html\n' +
-          'http://www.xinyangjlm.com/%E9%92%A2%E7%BB%93%E6%9E%84%E8%BD%A6%E6%A3%9A/index.html\n' +
-          'http://www.xinyangjlm.com/%E8%87%AA%E5%8A%A8%E6%84%9F%E5%BA%94%E9%97%A8/index.html\n' +
-          'http://www.xinyangjlm.com/%E5%A0%86%E7%A7%AF%E9%97%A8/index.html\n' +
-          'http://www.xinyangjlm.com/%E5%BF%AB%E9%80%9F%E9%97%A8/index.html\n' +
-          'http://www.xinyangjlm.com/%E7%94%B5%E5%8A%A8%E5%B9%B3%E7%A7%BB%E9%97%A8/index.html\n' +
-          'http://www.xinyangjlm.com/%E6%B0%B4%E6%99%B6%E5%8D%B7%E5%B8%98%E9%97%A8/index.html\n' +
-          'http://www.xinyangjlm.com/%E9%98%B2%E7%81%AB%E5%8D%B7%E5%B8%98%E9%97%A8/index.html'
-
+          'http://www.xinyangjlm.com/2016-11-21-9.html\n' +
+          'http://www.xinyangjlm.com/2016-11-21-8.html\n' +
+          'http://www.xinyangjlm.com/2016-11-21-7.html\n' +
+          'http://www.xinyangjlm.com/2017-02-04-6.html\n' +
+          'http://www.xinyangjlm.com/2016-11-21-5.html\n' +
+          'http://www.xinyangjlm.com/2016-11-21-4.html\n' +
+          'http://www.xinyangjlm.com/2016-11-21-3.html\n' +
+          'http://www.xinyangjlm.com/2016-11-21-2.html\n' +
+          'http://www.xinyangjlm.com/2016-11-21-1.html'
       }
     ]
   }
