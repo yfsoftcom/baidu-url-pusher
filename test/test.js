b32bd3e82f9f3125eb0fe48a8a91e3e61d1771ba
yfsoftcom <yfsoftcom@126.com>
2017-02-11 19:17:48 +0800

    update test

--- test/test.js
+++ test/test.js
@@ -2,18 +2,18 @@ import {baidu} from 'yf-website-tool'
 import config from '../key'
 import ejs from 'ejs'
 
-
 const emailTemplate = `
-  <h5>Domain: http://<%= domain%>百度解析结果</h5>
-  <h6>收录结果:</h6>
+  <h4>Domain: http://<%= domain%> 百度解析结果</h4>
+  <h4>收录结果:</h4>
+  <br/>
   <p><b><%= site%><b/></p>
-  <hr/>
-  <h6>关键字查询结果</h6>
+  <h4>关键字查询结果</h4>
   <% keywords.forEach(function(item){
     %>
-  <p><b><%= item.keyword %>:</b> <%= item.rank %></p>
+  <p><b><%= item.keyword %>:</b> <% if(!item.rank) {%>未找到<% }else{ %>在第 <%= item.rank.page %> 页<% }%></p>
   <%}) %>
 `
+
 const jobFun = async () => {
   for(let i = 0; i < config.baidu.sites.length; i++){
     let site = config.baidu.sites[i]
