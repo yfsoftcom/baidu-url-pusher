7d5bf63989e30750598d87927a25e7ef56ea6da3
yfsoftcom <yfsoftcom@126.com>
2017-02-11 16:26:02 +0800

    ok

--- test/test.js
+++ test/test.js
@@ -1,7 +1,5 @@
 import {baidu} from 'yf-website-tool'
 import config from '../key'
-import mailer from '../email'
-mailer.init(config.mail)
 
 const jobFun = async () => {
   for(let i = 0; i < config.baidu.sites.length; i++){
@@ -12,17 +10,6 @@ const jobFun = async () => {
       data.keywords = await baidu.crawler.checkKeywords(site)
       data.push = await baidu.pusher.pushUrls(site)
       console.log(JSON.stringify(data))
-      mailer.send(
-        {
-          to: site.result.to,
-          subject: 'BaiduToolMessage',
-          result: JSON.stringify(data)
-        }, (err, data) => {
-          if(err)
-            console.log(err)
-        }
-      )
-
     }catch(e){
       console.log(e)
     }
