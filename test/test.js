279505dc4547fa3c12fefcd9260be89b89297605
yfsoftcom <yfsoftcom@126.com>
2017-02-13 09:05:47 +0800

    push result convert to json

--- test/test.js
+++ test/test.js
@@ -24,6 +24,7 @@ const jobFun = async () => {
       data.push = await baidu.pusher.pushUrls(site)
       const result = ejs.render(emailTemplate, data)
       console.log(result)
+      console.log(data)
     }catch(e){
       console.log(e)
     }
